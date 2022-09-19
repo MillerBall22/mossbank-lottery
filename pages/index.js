e373a661ce411ebd2fd7a0b06eb69cccfda44f3f
Miller_Ball_22 <millerball22@gmail.com>
2022-09-18 19:00:15 -0600

    Cart Functioning

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState, useContext, useEffect } from 'react';
+import {StoreContext, ACTION_TYPES } from "../store/store-context";
 import TicketCard from '../components/card/ticket-card.component'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [tickets, setTickets] = useState('unavailable');
+  const {state, dispatch} = useContext(StoreContext);
+  const {cart} = state;
+
+  useEffect(() => {
+    if (cart[0].ticketQuantity !== 0 || cart[1].ticketQuantity !== 0 || cart[2].ticketQuantity !== 0) {
+      setTickets('can buy')
+    } else {
+      dispatch({
+            type: ACTION_TYPES.REMOVE_FROM_CART,
+            payload: {
+              ticketId: 'fiftyFiftyTickets'
+            },
+          });
+      setTickets('unavailable')
+    }
+  }, [cart])
+
+  const regularTicketsState = 'can buy';
+  
   return (
     <div className={styles.container}>
-      <TicketCard className='ticket-card' ticketTitle='1 Ticket' price='$60' image='/static/1Ticket.png' imageAlt='3 Tickets'/>
-      <TicketCard className='ticket-card' ticketTitle='3 Tickets' price='$150' image='/static/3Tickets.png' imageAlt='3 Tickets'/>
-      <TicketCard className='ticket-card' ticketTitle='10 Tickets' price='$400' image='/static/10Tickets.png' imageAlt='3 Tickets'/>
-      <TicketCard className='ticket-card' ticketTitle='50/50 Tickets' price='$20' image='/static/3Tickets.png' imageAlt='3 Tickets'/>
+      <TicketCard className='ticket-card' ticketId='singleTicket' ticketTitle='1 Ticket' price='$60' image='/static/1Ticket.png' imageAlt='3 Tickets' stateOfButton={regularTicketsState}/>
+      <TicketCard className='ticket-card' ticketId='threeTickets' ticketTitle='3 Tickets' price='$150' image='/static/3Tickets.png' imageAlt='3 Tickets' stateOfButton={regularTicketsState}/>
+      <TicketCard className='ticket-card' ticketId='tenTickets' ticketTitle='10 Tickets' price='$400' image='/static/10Tickets.png' imageAlt='3 Tickets' stateOfButton={regularTicketsState}/>
+      <TicketCard className='ticket-card' ticketId='fiftyFiftyTickets' ticketTitle='50/50 Tickets' price='$20' image='/static/3Tickets.png' imageAlt='3 Tickets' stateOfButton={tickets}/>
     </div>
   )
 }
